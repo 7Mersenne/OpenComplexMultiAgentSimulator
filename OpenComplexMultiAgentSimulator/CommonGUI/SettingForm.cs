ddfd1dba3eea3d2430ca69675a2cb8a0138014f4
瑛貴 北島 <k1010048@edu.cc.uec.ac.jp>
2018-12-31 17:38:41 +0900

    moved settingform's event function

--- OpenComplexMultiAgentSimulator/CommonGUI/SettingForm.cs
+++ OpenComplexMultiAgentSimulator/CommonGUI/SettingForm.cs
@@ -62,19 +62,5 @@ namespace OpenComplexMultiAgentSimulator
 
         }
 
-        private void comboBoxModel_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            foreach (var model in this.Models)
-            {
-                model.MyControl.Visible = false;
-                model.MyAnimationForm.Hide();
-            }
-
-            var model_enum = (ModelEnum)Enum.Parse(typeof(ModelEnum), this.comboBoxModel.Text, false);
-            this.MyModel = this.Models.First(model => model.MyModelEnum == model_enum);
-
-            this.MyModel.MyControl.Visible = true;
-            this.MyModel.MyAnimationForm.Show();
-        }
     }
 }
--- OpenComplexMultiAgentSimulator/CommonGUI/SettingFormEvent.cs
+++ OpenComplexMultiAgentSimulator/CommonGUI/SettingFormEvent.cs
@@ -11,6 +11,20 @@ namespace OpenComplexMultiAgentSimulator
     public partial class SettingForm : Form
     {
 
+        private void comboBoxModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (var model in this.Models)
+            {
+                model.MyControl.Visible = false;
+                model.MyAnimationForm.Hide();
+            }
+
+            var model_enum = (ModelEnum)Enum.Parse(typeof(ModelEnum), this.comboBoxModel.Text, false);
+            this.MyModel = this.Models.First(model => model.MyModelEnum == model_enum);
+
+            this.MyModel.MyControl.Visible = true;
+            this.MyModel.MyAnimationForm.Show();
+        }
     }
 
 }
